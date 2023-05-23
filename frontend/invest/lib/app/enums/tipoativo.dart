d3f33fdce177b2812111dbcc1d96f602685f1016
Pedro Ferraresi Silvani <95859358+Pedro-Ferraresi-Silvani@users.noreply.github.com>
2023-05-23 17:57:49 -0300

    fix Tipo Ativo

--- frontend/invest/lib/app/enums/tipoativo.dart
+++ frontend/invest/lib/app/enums/tipoativo.dart
@@ -1,25 +1,19 @@
 enum TipoAtivos {
   acoes('Ações'),
-  fundosImobiliarios('Fundos_Imobiliários');
+  fundosImobiliarios('Fundos_Imobiliários'),
+  fundosInvestimentos('Fundos_Investimentos'),
+  fiagro('Fiagro'),
+  stocks('Stocks'),
+  reits('Reits'),
+  eTFsNacionais('ETFs_Nacionais'),
+  criptomoedas('Criptomoedas'),
+  tesouroDireto('Tesouro_Direto'),
+  cDB('CDB'),
+  lCIeLCA('LCI_e_LCA'),
+  crieCra('Cri_e_Cra'),
+  debendures('Debendures'),
+  bDRs('BDRs');
 
   final String value;
   const TipoAtivos(this.value);
-
-  // Map tiposAtivos = {
-  //   // '1': 'Ações',
-  //   // '2': 'Fundos_Imobiliários',
-  //   // '3': 'Fundos_Investimentos',
-  //   // '4': 'Fiagro',
-  //   // '5': 'Stocks',
-  //   // '6': 'Reits',
-  //   // '7': 'ETFs_Nacionais',
-  //   // '8': 'ETFs_Internacionais',
-  //   // '9': 'Criptomoedas',
-  //   // '10': 'Tesouro_Direto',
-  //   // '11': 'CDB',
-  //   // '12': 'LCI_e_LCA',
-  //   // '13': 'Cri_e_Cra',
-  //   // '14': 'Debendures',
-  //   // '15': 'BDRs'
-  // };
 }
